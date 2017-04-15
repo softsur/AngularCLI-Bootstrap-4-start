1a59d30e5b2252e986429469d4ea9766837f20ce
Unknown <raomaster@gmail.com>
2017-04-15 17:49:45 -0300

    Modify Navbar to Toggle

--- src/app/estructura/navbar/navbar.component.ts
+++ src/app/estructura/navbar/navbar.component.ts
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent {
     titulo = 'Softsur.cl';
-    nosotros = 'Nosotros';
+    nosotros = 'Servicios';
     contactanos = 'Contactanos';
     buscar = 'buscar';
     inicio = 'Inicio';
     buscar_place = 'Buscar en la web';
     Entrada = 'hola';
+
+    isShow = false;
+       // store state
+    toggleState() { // click handler
+        let bool = this.isShow;
+        this.isShow = bool === false ? true : false;
+    }
 }
